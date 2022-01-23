fdb3f6e60a3c47104f07da794c941f4927f1b099
Eduardo Morsch Gadelha <eduardo.morsch.gadelha@gmail.com>
2022-01-23 23:16:35 +0100

    Added stock_market.py

--- stock_market.py
+++ stock_market.py
@@ -2,6 +2,14 @@ import streamlit as st
 import time
 import numpy as np
 
+import pandas as pd
+
+#Data Source
+import yfinance as yf
+
+#Data viz
+import plotly.graph_objs as go
+
 st.title('Calculadora de valores')
 
 def juros_compostos(j, n):
@@ -9,6 +17,40 @@ def juros_compostos(j, n):
 
 def juros_simples(j, n):
     return j * n
+
+stocks = st.sidebar.multiselect('Escolha a/as ações que você deseja:',('PBR','NU','UBER','MSFT','AAPL','ERJ'))
+for simbolo in stocks:
+    data = yf.download(tickers=simbolo, period='5d', interval='5m')
+    #declare figure
+    fig = go.Figure()
+
+    #Candlestick
+    fig.add_trace(go.Candlestick(x=data.index,
+                open=data['Open'],
+                high=data['High'],
+                low=data['Low'],
+                close=data['Close'], name = 'market data'))
+
+    # Add titles
+    fig.update_layout(
+    title=simbolo + ' live share price evolution',
+    yaxis_title='Stock Price (USD per Shares)')
+
+    # X-Axes
+    fig.update_xaxes(
+        rangeslider_visible=True,
+        rangeselector=dict(
+        buttons=list([
+                dict(count=15, label="15m", step="minute", stepmode="backward"),
+                dict(count=45, label="45m", step="minute", stepmode="backward"),
+                dict(count=1, label="HTD", step="hour", stepmode="todate"),
+                dict(count=3, label="3h", step="hour", stepmode="backward"),
+                dict(step="all")
+            ])
+        )
+    )
+    st.plotly_chart(fig, use_container_width=True)
+
 
 tipo_juros = st.sidebar.radio('Tipo de juros:',('Simples','Compostos'))
 
